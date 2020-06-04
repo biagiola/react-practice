6b6b45b32cc6ccbec9b5d7ed12d5f2ba72c2f4dc
David <41311088+biagiola@users.noreply.github.com>
2020-06-04 19:46:16 -0400

    Add files via upload

---
+++ src/components/04-exampleFour.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Four extends Component {
+    
+    method1(a, b) {
+        return a + b;
+    }
+
+    render() {
+        return (
+            <div>
+                <div onClick></div>
+            </div>
+        )
+    }
+}
+
+export default Four;
---
+++ src/components/05-map-filter.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class State extends Component {
+    state = {
+        lista: [5,5,4,8,6,2]
+    }
+    render() {
+        return (
+            <div className='box'>
+                <div>Component 5</div>
+                <span> this.state.lista = </span>
+                <span>{ this.state.lista }</span>
+                <br/>
+                
+                <span>this.state.lista.map = </span>
+                {
+                    this.state.lista.map( e => {
+                        return <span>{ e }</span>
+                    })
+                }<br/>
+
+                <span>this.state.lista.filter = </span>
+                <span>{this.state.lista.filter( e => e > 5)}</span>
+                <br/>
+
+                <span>this.state.lista.filter.map = </span>
+                <span>{
+                    this.state.lista.filter( e => e > 5).map( e => {
+                    return <span>{ e }</span>
+                    })
+                }</span>
+            </div>
+        )
+    }
+}
+
+export default State;
